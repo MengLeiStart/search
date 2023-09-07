beadb31ee67a914af1f117c793804233e5eab49f
MengLeiStart <728093780@qq.com>
2023-09-07 10:06:02 +0800

    初始版本，修改了查找到值不直接返回结果，仍然继续查找

---
+++ 路径/Form.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace 路径
+{
+    partial class Form
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_Search = new System.Windows.Forms.Button();
+            this.lb_Name = new System.Windows.Forms.Label();
+            this.txt_Input = new System.Windows.Forms.TextBox();
+            this.lb_Result = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bt_Search
+            // 
+            this.bt_Search.Location = new System.Drawing.Point(490, 178);
+            this.bt_Search.Name = "bt_Search";
+            this.bt_Search.Size = new System.Drawing.Size(78, 38);
+            this.bt_Search.TabIndex = 0;
+            this.bt_Search.Text = "搜索";
+            this.bt_Search.UseVisualStyleBackColor = true;
+            this.bt_Search.Click += new System.EventHandler(this.bt_Search_Click);
+            // 
+            // lb_Name
+            // 
+            this.lb_Name.AutoSize = true;
+            this.lb_Name.Location = new System.Drawing.Point(249, 190);
+            this.lb_Name.Name = "lb_Name";
+            this.lb_Name.Size = new System.Drawing.Size(67, 15);
+            this.lb_Name.TabIndex = 1;
+            this.lb_Name.Text = "节点名称";
+            // 
+            // txt_Input
+            // 
+            this.txt_Input.Location = new System.Drawing.Point(322, 187);
+            this.txt_Input.Name = "txt_Input";
+            this.txt_Input.Size = new System.Drawing.Size(149, 25);
+            this.txt_Input.TabIndex = 2;
+            // 
+            // lb_Result
+            // 
+            this.lb_Result.AutoSize = true;
+            this.lb_Result.Location = new System.Drawing.Point(353, 257);
+            this.lb_Result.Name = "lb_Result";
+            this.lb_Result.Size = new System.Drawing.Size(55, 15);
+            this.lb_Result.TabIndex = 3;
+            this.lb_Result.Text = "result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lb_Result);
+            this.Controls.Add(this.txt_Input);
+            this.Controls.Add(this.lb_Name);
+            this.Controls.Add(this.bt_Search);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_Search;
+        private System.Windows.Forms.Label lb_Name;
+        private System.Windows.Forms.TextBox txt_Input;
+        private System.Windows.Forms.Label lb_Result;
+    }
+}
+
---
+++ 路径/Form.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace 路径
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        Menus root;
+        string result = "";
+        public Form()
+        {
+            InitializeComponent();
+
+        }
+        //节点
+        public class Menus
+        {
+            public string Name { get; set; }
+            public Menus Parent { get; set; }
+            public List<Menus> SubMenus { get; set; }
+            public Menus(string name)
+            {
+                Name = name;
+                SubMenus = new List<Menus>();
+            }
+        }
+        public Menus BuildMenu(string txt)
+        {
+            //把需要用来分割得符号放入数组
+            string[] str = { "(", ")", "," };
+            //这里用于分割字符串但是分割完后保留分割符
+            for (int i = 0; i < str.Length; i++)
+            {
+                //把每一个分割符前后加上@符号用于后续保留分隔符
+                txt = txt.Replace(str[i], "@" + str[i] + "@");
+            }
+            string[] parts = txt.Split(new char[] { '@', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            //创建根节点
+            root = new Menus(parts[0]);
+            //根节点没有父节点
+            root.Parent = null;
+            for (int i = 2; i < parts.Length - 1; i++)
+            {              
+               Menus son = new Menus(parts[i]);
+               son.Parent = root;
+               root.SubMenus.Add(son);
+               if (parts[i + 1] == "(")
+               {
+                   for (int j = i + 2; j < parts.Length; j++)
+                   {
+                  
+                       if (parts[j] == ")")
+                       {
+                           i = j;
+                           break;
+                       }
+                       Menus grandSon = new Menus(parts[j]);
+                       grandSon.Parent = son;
+                       son.SubMenus.Add(grandSon);
+                   }
+               }                
+            }
+            return root;
+        }
+        string GetPath(string name, Menus menu)
+        {
+            //如果是根节点直接返回名字
+            if (name == menu.Name)
+            {
+                result += menu.Name;
+                return result;
+            }
+            else
+            {
+                //遍历根节点的子节点
+                foreach (var tmp in menu.SubMenus)
+                {
+                    if (!result.Equals("")) {
+                        break;
+                    }
+                    if (tmp.Name == name)
+                    {                      
+                        if (menu.Parent != null)
+                        {
+                            //返回孙子节点得查找路径
+                            result += menu.Parent.Name+"->"+ menu.Name + "->" + tmp.Name;
+                            return result;
+                        }
+                        else {
+                            //返回子节点得查找路径
+                            result +=  menu.Name + "->" + tmp.Name;
+                            return result;
+                        }                       
+                    }
+                    else
+                    {
+                        //判断根节点的子节点有无子节点
+                        if (tmp.SubMenus.Count > 0)
+                        {                       
+                            //如果有子节点进行递归
+                            result = GetPath(name, tmp);
+                        }
+                    }
+                }
+                //返回查找结果
+                return result;
+            }
+        }
+        private void bt_Search_Click(object sender, EventArgs e)
+        {
+            //每次搜索前清空路径值
+            result = "";
+            Menus rot = BuildMenu("Root(MenuA(SA1,SA2),MenuB(LB1,LB2,LB3),MenuC)");
+            string name = txt_Input.Text;
+            string pathText = GetPath(name, rot);
+            lb_Result.Text = pathText;
+        }
+    }
+}
